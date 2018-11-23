6f281161193a96986b866431a52862c6ce6a2f01
Andy Yang <onionyoung80@gmail.com>
2018-11-23 21:00:42 +0800

    Init Project

---
+++ ios-exam/ios-exam/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-exam
+//
+//  Created by Andy Yang on 2018/11/23.
+//  Copyright © 2018 Andy Yang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
